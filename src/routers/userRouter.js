8cd8b5dab13449179c035b75378b1b7aedd0e0df
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-03-18 21:38:29 -0300

    User router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,51 +1,11 @@
-import express from "express"
+import express from 'express'
+import getUserController from '../controllers/user/getUserController.js'
+import createUserController from '../controllers/user/createUserController.js'
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.json({
-        name: "Robert",
-        email: "robert@email.com",
-        avatar: "http://github.com/Robert-Cortez-Rudi.png"
-    })
-})
-
-router.post("/", (req, res) => {
-    res.json({
-        message: "Usuário criado com sucesso!",
-        user: {
-            name: "Robert",
-            email: "robert@email.com",
-            avatar: "http://github.com/Robert-Cortez-Rudi.png"
-        }
-    })
-})
-
-router.put("/", (req, res) => {
-    res.json({
-        message: "Usuário atualizado com sucesso!",
-        info: {
-            name: "Roberto",
-            email: "Roberto@email.com",
-            avatar: "http://github.com/Robert-Cortez-Rudi.png"
-        }
-    })
-})
-
-router.delete("/", (req, res) => {
-    res.json({
-        message: "Usuário deletado do sistema!!"
-    })
-})
-
-router.patch("/", (req, res) => {
-    res.json({
-        message: "Atualizações das informações de: nome, foto",
-        updates: {
-            name: "Leandro",
-            avatar: "http://github.com/Leandro.png"
-        }
-    })
-})
+router.get('/:id', getUserController)
+router.post('/', createUserController)
+//...continua
 
 export default router
