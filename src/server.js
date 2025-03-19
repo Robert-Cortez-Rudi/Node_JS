525b2f71fdbbb2f497ebd4e25ab0410c55534bdd
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-03-18 21:40:11 -0300

    main server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
-// const express = require('express') // O require() é a função usada para importar módulos
-import express from "express"
-import userRouter from "./routers/userRouter.js"
-import imovelRouter from "./routers/imovelRouter.js"
+//const express = require('express')
+import express from 'express'
+import userRouter from './routers/userRouter.js'
+import propertyRouter from './routers/propertyRouter.js'
+
 const app = express()
 const port = 3000
 
-app.use("/user", userRouter)
-app.use("/imovel", imovelRouter)
+// Middleware
+app.use(express.json()) // faz o parse do json e transforma em objeto no req.body
+
+app.use('/user', userRouter)
+app.use('/property', propertyRouter)
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
 })
-
